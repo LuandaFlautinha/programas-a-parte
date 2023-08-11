a76aa2f8cd982109db522664aecc0ec169a83043
LuandaFlautinha <hluan1414@outlook.com>
2023-08-11 15:36:07 -0400

    Create jogo-da-cobra.py

---
+++ jogo-da-cobra.py
@@ -0,0 +1,33 @@
+#configurações iniciais
+import pygame
+import random
+
+pygame.init()
+pygame.display.set_caption("Jogo da Cobrinha Bolado")
+largura, altura = 800, 600
+pygame.display.set_mode((largura,altura))
+
+#cores RGB
+preto = (0, 0, 0)
+branca = (255, 255, 255)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+
+#parametros da cobrinha
+
+
+#criar um loop infinito
+
+
+    #desenhar os objetos do jogo na tela
+        #pontuação
+        #cobrinha
+        #cobrinha
+
+    #criar a lógica de terminar o jogo
+        #cobra bateu na parede
+        #cobra bateu nela mesma
+
+    #pegar a interação do usuário
+        #fechou a tela
+        #apertou a tecla do teclado para mover a cobra
